679ca2249400a440a989812b9570d958a02b720a
SyperOlao <olay1234538@gmail.com>
2025-12-12 18:51:49 +0300

    Initial commit

---
+++ core/Dice.cpp
@@ -0,0 +1,12 @@
+//
+// Created by SyperOlao on 10.12.2025.
+//
+
+#include "Dice.h"
+
+// “2d6+2,3d10”
+int Dice::roll(std::string param) {
+    
+    return 0;
+}
+
---
+++ core/Dice.h
@@ -0,0 +1,18 @@
+//
+// Created by SyperOlao on 10.12.2025.
+//
+
+#ifndef DICE_DICE_H
+#define DICE_DICE_H
+#import <string>
+
+class Dice {
+public:
+    // “2d6+2,3d10”
+    int roll(std::string param);
+
+
+};
+
+
+#endif //DICE_DICE_H
